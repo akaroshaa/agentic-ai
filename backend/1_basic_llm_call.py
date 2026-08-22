267f682889d9038c359b98fd4216c94e4e7457b9
akaroshaa <akaroshaa@gmail.com>
2026-08-22 18:49:39 +0530

    added basic llm setup code

---
+++ backend/1_basic_llm_call.py
@@ -0,0 +1,4 @@
+import llms
+
+llm_result = llms.call_azure_openai_llm("What is the capital of France?")
+print(llm_result)
---
+++ backend/llms.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_openai import AzureChatOpenAI
+
+load_dotenv()  # Load environment variables from .env file
+
+def call_azure_openai_llm(user_input):
+
+    llm = AzureChatOpenAI(
+        azure_endpoint="https://crook-mt496dmp-eastus2.openai.azure.com/",
+        azure_deployment="sample-gpt-4o-deployment",
+        api_key=os.environ.get("azure_openai_api_key"),
+        api_version="2024-12-01-preview",
+        temperature=0.5,
+        top_p=1.0,
+        max_tokens=4096,
+    )
+
+    # Prepare messages
+    messages = [
+        SystemMessage(content="You are a helpful assistant."),
+        HumanMessage(content=user_input),
+    ]
+
+    # Invoke the model
+    response = llm.invoke(messages)
+
+    return response.content
